a23a2cadd820b0b2a0683d72b57002d75fa86cbf
unknown <jules.gioux@gmail.com>
2020-06-10 16:49:05 +0200

    ex2

---
+++ PHP_Exercice2/index.php
@@ -0,0 +1,16 @@
+<?php 
+    $today = date("m-d-y"); 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Partie 9 Exercice 1</title>
+</head>
+<body>
+    <p>
+        <?= $today ?>
+    </p>
+</body>
+</html>
