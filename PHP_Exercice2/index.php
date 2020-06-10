734a8b071935d0ebeb522402207a0fba9a11ee90
unknown <jules.gioux@gmail.com>
2020-06-10 16:51:49 +0200

    ex2

--- PHP_Exercice2/index.php
+++ PHP_Exercice2/index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Partie 9 Exercice 1</title>
+    <title>PHP Partie 9 Exercice 2</title>
 </head>
 <body>
     <p>
