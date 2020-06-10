6b28c97b15a50b3e3983a9ecf786bb28797fbb66
unknown <jules.gioux@gmail.com>
2020-06-10 16:54:04 +0200

    ex3

---
+++ PHP_Exercice3/index.php
@@ -0,0 +1,17 @@
+<?php 
+    setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+    $today = (strftime("%A %d %B")); 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Partie 9 Exercice 3</title>
+</head>
+<body>
+    <p>
+        <?= $today ?>
+    </p>
+</body>
+</html>
